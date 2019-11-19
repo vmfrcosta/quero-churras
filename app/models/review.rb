d3e6a1f5289038a40a7a076a0da130abd20938fd
vinicius <vmfrcosta@gmail.com>
2019-11-19 18:48:17 -0300

    added associations and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :grill
+  validates :stars, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }, numericality: { only_integer: true }
+  validates :content, presence: true, allow_blank: true
+  validates :user, presence: true
+  validates :grill, presence: true
 end
