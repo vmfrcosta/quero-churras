aaba3ca19c27884802cc72892bc2ee28edfeca33
vinicius <vmfrcosta@gmail.com>
2019-11-20 15:30:07 -0300

    added modal for booking

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,10 @@
 import "bootstrap";
 import "../plugins/flatpickr"
+
+const submitBooking = document.querySelector('#submit-booking')
+
+submitBooking.addEventListener('click', (event) => {
+	event.preventDefault()
+})
+
+console.log(submitBooking)
