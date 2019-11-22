53da166e506fdd9efcf5b32f9f7122419ebb2f9b
carlos silva <carlosantonio.dsilva@gmail.com>
2019-11-22 10:42:24 -0300

    seed e calendario

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,16 +2,9 @@ import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css"
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
-flatpickr(".datepicker", {
-  enableTime: false,
+flatpickr(".range_start", {
   altInput: true,
-  allowInput: true,
-  confirmText: "OK ",
-  confirmIcon: "<i class='fa fa-check'></i>", // your icon's html, if you wish to override
-  confirmText: "OK ",
-  showAlways: false,
-  theme: "dark", // or "dark"
+  plugins: [new rangePlugin({ input: ".range_end"})]
 })
-
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
   has_many :reviews
   # validates :last_name, presence: true
   # validates :first_name, presence: true
+  mount_uploader :photo, PhotoUploader
 end
